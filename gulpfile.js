7a942b62e9e52af5531fc7633a7378444499144d
Ali Hasanov <nan.fgbs@gmail.com>
2025-06-10 12:06:00 +0400

    chore: 🔧 customized and extended base library - p.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 const {src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const purgecss = require('gulp-purgecss')
+const plumber = require('gulp-plumber')
 
 function buildStyles() {
     return src('./scss/**/*.scss')
+        .pipe(plumber({
+          errorHandler: function (err) {
+            console.error('SCSS Compilation Error:', err.message);
+            this.emit('end');
+          }
+        }))
         .pipe(sass())
         .pipe(purgecss({ content: ['*.html'] }))
         .pipe(dest('css'))
